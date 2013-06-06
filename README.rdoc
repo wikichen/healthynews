682cfa314853bea78820045767bf6e8435770d94
wikichen <wikichen@server.fake>
2013-06-05 23:26:22 -0700

    First commit

---
+++ README.rdoc
@@ -0,0 +1,3 @@
+## Healthy News
+
+A social news site at the intersection of healthcare and technology.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Healthynews::Application.config.secret_token = '3ac9a0d5eba69536a07c72b23652da77f8e71a1162a6196215b8f0b7cfd68d46242968ad3027400ba11359e047c51a68aa3bea49e90d178fdfe2d89cfad5ce6a'
