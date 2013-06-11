579603ab5fb9608b94e63e1da23684b47e91f8df
wikichen <wikichen@server.fake>
2013-06-11 16:37:22 -0700

    Add basic vote validations

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,6 +1,8 @@
 class VotesController < ApplicationController
   def create
-    @vote = current_user.votes.create(params[:vote])
+    @vote = current_user.votes.where(:post_id => params[:vote][:post_id]).first
+    @vote ||= current_user.votes.create(params[:vote])
+    @vote.update_attributes(:vote => params[:vote][:vote])
     redirect_to :back
   end
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,4 +3,6 @@ class Vote < ActiveRecord::Base
   belongs_to :post
 
   attr_accessible :post_id, :user_id, :vote
+
+  validates :user_id, :uniqueness => { :scope => :post_id }
 end
