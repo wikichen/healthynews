4ee80bf727f3e37560ca70e2b4664cf7227ffbb0
wikichen <wikichen@server.fake>
2013-06-14 23:14:10 -0700

    Refactor tests

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -6,18 +6,32 @@ describe "Pages" do
 
   subject { page }
 
+  shared_examples_for "all pages" do
+    it { should have_content(heading) }
+    it { should have_title(full_title(page_title)) }
+  end
+
   describe "Index page" do
     before { visit root_path }
+    let(:heading) { 'Healthy News' }
+    let(:page_title) { '' }
 
-    it { should have_title(full_title('')) }
-    it { should have_content('Healthy News') }
+    it_should_behave_like 'all pages'
     it { should_not have_title('- Home') }
   end
 
   describe "About page" do
     before { visit about_path }
+    let(:heading) { 'About' }
+    let(:page_title) { 'About' }
 
-    it { should have_title(full_title('About')) }
-    it { should have_content('About') }
+
+    it_should_behave_like 'all pages'
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link 'Register'
+    expect(page).to have_title(full_title('Register'))
   end
 end
