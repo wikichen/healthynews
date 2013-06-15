30f4189ccfd55791c17eea70439af726f497a9cd
wikichen <wikichen@server.fake>
2013-06-15 00:25:54 -0700

    Add index to username

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130609072935) do
+ActiveRecord::Schema.define(:version => 20130615071354) do
 
   create_table "comments", :force => true do |t|
     t.text     "body"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(:version => 20130609072935) do
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+  add_index "users", ["username"], :name => "index_users_on_username", :unique => true
 
   create_table "votes", :force => true do |t|
     t.boolean  "vote"
